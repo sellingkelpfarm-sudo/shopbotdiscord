80a45e7d32c787f0399ccf39d69c97a0b51bf692
sellingkelpfarm-sudo <sellingkelpfarm@gmail.com>
2026-03-07 23:54:31 +0700

    Update card_system.py

--- card_system.py
+++ card_system.py
@@ -148,7 +148,7 @@ class CancelConfirm(discord.ui.View):
 # CARD MODAL
 # ==============================
 
-class CardModal(discord.ui.Modal, title="💳 THANH TOÁN CARD"):
+class CardModal(discord.ui.Modal, title="💳 THANH TOÁN CARD [Lưu ý: giá tiền phải đúng với mức giá bán của hiện tại. Nạp sai mệnh giá sẽ không hoàn lại tiền!]"):
 
     telco = discord.ui.TextInput(label="Nhà mạng (VIETTEL/MOBI/VINA)")
     serial = discord.ui.TextInput(label="Serial")
@@ -408,3 +408,4 @@ class CardSystem(commands.Cog):
 
 async def setup(bot):
     await bot.add_cog(CardSystem(bot))
+
