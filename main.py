d07fedc5d169ceeaf89efa149e9b055a995f8b58
sellingkelpfarm-sudo <sellingkelpfarm@gmail.com>
2026-03-08 01:56:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-print(f"Bot đã đăng nhập: {bot.user}")
+    print(f"Bot đã đăng nhập: {bot.user}")
 
 @bot.event
 async def setup_hook():
-await bot.load_extension("sell_system")
-await bot.load_extension("MenuRole")
-await bot.load_extension("card_system")
+    await bot.load_extension("sell_system")
+    await bot.load_extension("MenuRole")
+    await bot.load_extension("card_system")
 
 TOKEN = os.getenv("TOKEN")
 bot.run(TOKEN)
